810e0f52e7d89867e82aeee18caf1dea0fe4b018
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-07-21 19:02:07 -0500

    Updates

---
+++ precache-manifest.566f5bdf40f98a4beb9896f547424a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dda0939eda3edc37f282a096bf49daa",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "1306e7eaad9c5d037fd0",
+    "url": "/react-calculator/static/css/main.5078290d.chunk.css"
+  },
+  {
+    "revision": "a4b2fe6b1533d35ce60e",
+    "url": "/react-calculator/static/js/2.abc4e447.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator/static/js/2.abc4e447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1306e7eaad9c5d037fd0",
+    "url": "/react-calculator/static/js/main.0bee374c.chunk.js"
+  },
+  {
+    "revision": "e0410226154151dcd210",
+    "url": "/react-calculator/static/js/runtime-main.f5cd65e1.js"
+  }
+]);
